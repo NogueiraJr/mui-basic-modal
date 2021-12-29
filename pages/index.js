a813730f0f1695930d49a7676598521b2eb0ce0a
Admilson Manoel Nogueira Júnior <nogueirajr@hotmail.com>
2021-12-29 13:38:00 +0000

    Alter text in button

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function BasicModal() {
 
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
+      <Button onClick={handleOpen}>Modaal openen</Button>
       <Modal
         open={open}
         onClose={handleClose}
